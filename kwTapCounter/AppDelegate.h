af79a497ff9f249b9b250a87eccfa0061cac833a
Sharmila <sharmila@felix-p-01.local>
2016-09-08 14:20:21 +0530

    Initial Commit

---
+++ kwTapCounter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kwTapCounter
+//
+//  Created by Sharmila on 08/09/16.
+//  Copyright © 2016 Sharmila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kwTapCounter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kwTapCounter
+//
+//  Created by Sharmila on 08/09/16.
+//  Copyright © 2016 Sharmila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
